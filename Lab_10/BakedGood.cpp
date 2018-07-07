ccd301ec59d695a9019abe4f62e2b1fb8ac7e3e6
Jordan G <jorg2332@gmail.com>
2018-07-07 12:11:32 -0600

    Added BakedGood abstract class

---
+++ Lab_10/BakedGood.cpp
@@ -0,0 +1,12 @@
+#include "stdafx.h"
+#include "BakedGood.h"
+
+
+BakedGood::BakedGood()
+{
+}
+
+
+BakedGood::~BakedGood()
+{
+}
---
+++ Lab_10/BakedGood.h
@@ -0,0 +1,11 @@
+#ifndef BAKEDGOOD_H
+#define BAKEDGOOD_H
+
+class BakedGood
+{
+	public:
+		BakedGood();
+		~BakedGood();
+};
+
+#endif
