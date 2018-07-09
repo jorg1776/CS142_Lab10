e7ebc04ec20d5b36d67f8b0b034990f9a7961d22
Jordan G <jorg2332@gmail.com>
2018-07-08 23:12:27 -0600

    removed default initializer for pure virtual functions

--- Lab_10/Cake.h
+++ Lab_10/Cake.h
@@ -9,7 +9,7 @@ class Cake : public BakedGood
 		Cake();
 		~Cake();
 		virtual string ToString() = 0;
-		virtual float GetDiscountedPrice(int quantity = 1) = 0;
+		virtual float GetDiscountedPrice(int) = 0;
 	protected:
 		string flavor;
 		string frosting;
